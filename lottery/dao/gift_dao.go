0260d66de2c7dbb3f546034189e54ebdf3779095
cnman <marsedely@163.com>
2019-10-06 00:07:16 +0800

    --

--- lottery/dao/gift_dao.go
+++ lottery/dao/gift_dao.go
@@ -1,9 +1,9 @@
 package dao
 
 import (
+	"github.com/cnmac/golearning/lottery/comm"
 	"github.com/cnmac/golearning/lottery/models"
 	"github.com/go-xorm/xorm"
-	"log"
 )
 
 type GiftDao struct {
@@ -11,51 +11,109 @@ type GiftDao struct {
 }
 
 func NewGiftDao(engine *xorm.Engine) *GiftDao {
-	return &GiftDao{engine}
+	return &GiftDao{
+		engine: engine,
+	}
 }
 
-func (d GiftDao) Get(id int) *models.LtGift {
-	gift := &models.LtGift{Id: id}
-	ok, e := d.engine.Get(gift)
-	if ok && e == nil {
-		return gift
+func (d *GiftDao) Get(id int) *models.LtGift {
+	data := &models.LtGift{Id: id}
+	ok, err := d.engine.Get(data)
+	if ok && err == nil {
+		return data
 	} else {
-		gift.Id = 0
-		return gift
+		data.Id = 0
+		return data
 	}
 }
 
-func (d GiftDao) GetAll() []models.LtGift {
-	gifts := make([]models.LtGift, 0)
-	err := d.engine.Asc("sys_status", "displayorder").Find(&gifts)
+func (d *GiftDao) GetAll() []models.LtGift {
+	datalist := make([]models.LtGift, 0)
+	err := d.engine.
+		Asc("sys_status").
+		Asc("displayorder").
+		Find(&datalist)
 	if err != nil {
-		log.Println("gift_dao.GetAll error=", err)
-		return gifts
+		return datalist
+	} else {
+		return datalist
 	}
-	return gifts
 }
 
-func (d GiftDao) CountAll() int64 {
-	num, e := d.engine.Count(&models.LtGift{})
-	if e != nil {
+func (d *GiftDao) CountAll() int64 {
+	num, err := d.engine.
+		Count(&models.LtGift{})
+	if err != nil {
 		return 0
 	} else {
 		return num
 	}
 }
 
-func (d GiftDao) Delete(id int) error {
-	gift := &models.LtGift{Id: id, SysStatus: 1}
-	_, e := d.engine.Id(id).Update(gift)
-	return e
+//func (d *GiftDao) Search(country string) []models.LtGift {
+//	datalist := make([]models.LtGift, 0)
+//	err := d.engine.
+//		Where("country=?", country).
+//		Desc("id").
+//		Find(&datalist)
+//	if err != nil {
+//		return datalist
+//	} else {
+//		return datalist
+//	}
+//}
+
+func (d *GiftDao) Delete(id int) error {
+	data := &models.LtGift{Id: id, SysStatus: 1}
+	_, err := d.engine.Id(data.Id).Update(data)
+	return err
 }
 
-func (d GiftDao) Update(data *models.LtGift, columns []string) error {
-	_, e := d.engine.Id(data.Id).MustCols(columns...).Update(data)
-	return e
+func (d *GiftDao) Update(data *models.LtGift, columns []string) error {
+	_, err := d.engine.Id(data.Id).MustCols(columns...).Update(data)
+	return err
 }
 
-func (d GiftDao) Create(gift *models.LtGift) error {
-	_, e := d.engine.Insert(gift)
-	return e
+func (d *GiftDao) Create(data *models.LtGift) error {
+	_, err := d.engine.Insert(data)
+	return err
+}
+
+// 获取到当前可以获取的奖品列表
+// 有奖品限定，状态正常，时间期间内
+// gtype desc， displayorder asc
+func (d *GiftDao) GetAllUse() []models.LtGift {
+	now := comm.NowUnix()
+	datalist := make([]models.LtGift, 0)
+	err := d.engine.
+		Cols("id", "title", "prize_num", "left_num", "prize_code",
+			"prize_time", "img", "displayorder", "gtype", "gdata").
+		Desc("gtype").
+		Asc("displayorder").
+		Where("prize_num>=?", 0).    // 有限定的奖品
+		Where("sys_status=?", 0).    // 有效的奖品
+		Where("time_begin<=?", now). // 时间期内
+		Where("time_end>=?", now).   // 时间期内
+		Find(&datalist)
+	if err != nil {
+		return datalist
+	} else {
+		return datalist
+	}
+}
+
+func (d *GiftDao) IncrLeftNum(id, num int) (int64, error) {
+	r, err := d.engine.Id(id).
+		Incr("left_num", num).
+		//Where("left_num=?", num).
+		Update(&models.LtGift{Id: id})
+	return r, err
+}
+
+func (d *GiftDao) DecrLeftNum(id, num int) (int64, error) {
+	r, err := d.engine.Id(id).
+		Decr("left_num", num).
+		Where("left_num>=?", num).
+		Update(&models.LtGift{Id: id})
+	return r, err
 }
